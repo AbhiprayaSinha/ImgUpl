7d3183f1c6829e6f01678ae7e02c5fa9c3aca448
Abhipraya Sinha <abhiprayasinha@gmail.com>
2024-02-13 15:48:49 +0530

    added both

---
+++ frontend/src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const logoutHandler = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">ImgUpl</span>
+        </Link>
+      </div>
+      
+      <div className="topbarRight">
+       
+        <button className="logoutButton" onClick = {logoutHandler}>Logout</button>
+        
+      </div>
+    </div>
+  );
+}
